1af22793e906b6af9f18218b893595129e25fc80
ratnadeepak1981 <ratnadeepak1981@gmail.com>
2026-05-19 15:36:21 +0530

    Add files via upload
    C# Day 3 activity 1 to 4

---
+++ Day3_Activity2_Console/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Day3_Activity2_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number;
+            Console.Write("Enter the Number between 1 to 12 : ");
+            if (int.TryParse(Console.ReadLine(), out Number))
+            {
+                if (Number < 1 || Number > 12)
+                {
+                    Console.WriteLine("Number out of range (1-12)");
+                }
+                else
+                {
+                    for(int i = 1; i <= 12; i++)
+                    {
+                        int Multiply=i * Number;
+                        Console.WriteLine($"{Number} X {i} = {Multiply} ");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Number");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day3_Activity3_Console/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Day3_Activity3_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string SecretWord = "";
+            do
+            {
+                Console.Write("Enter the secret word : ");
+                SecretWord = Console.ReadLine();
+            }
+            while(SecretWord!="CSharp");
+
+            Console.WriteLine("You found the Secret Message sucessfully");
+        }
+        
+    }
+}
